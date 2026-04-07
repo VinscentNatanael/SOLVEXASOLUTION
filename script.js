a89cbe6c2080bd55b5956155b383b5b3eccf252d
VinscentNatanael <vinscentlam@gmail.com>
2026-04-07 15:15:56 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Navbar effect
+window.addEventListener("scroll", () => {
+  document.getElementById("navbar")
+    .classList.toggle("scrolled", window.scrollY > 50);
+});
+
+// Animasi muncul
+const cards = document.querySelectorAll(".card");
+
+window.addEventListener("scroll", () => {
+  cards.forEach(card => {
+    const top = card.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100){
+      card.classList.add("show");
+    }
+  });
+});
